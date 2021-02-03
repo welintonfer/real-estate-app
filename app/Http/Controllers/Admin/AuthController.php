10adf769c63ba8ad0f09d9757b88995552eae311
welintonfer <portfolio.fernandes@gmail.com>
2021-02-03 16:55:52 +0000

    first push

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LaraDev\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use LaraDev\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('admin.index');
+    }
+
+    public function home()
+    {
+        return view('admin.dashboard');
+    }
+}
